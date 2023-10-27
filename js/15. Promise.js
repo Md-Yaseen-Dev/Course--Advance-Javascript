09f3bd9c81436b4eb522886c48d8c9bc468cf1bc
Mohammad Yaseen <ysn6040@gmail.com>
2023-10-28 00:34:06 +0530

    promise completed

--- js/15. Promise.js
+++ js/15. Promise.js
@@ -103,4 +103,72 @@ let promise2 = new Promise(function(resolve, reject) {
     setTimeout(() => resolve("doney!"), 1000);
   });
   
-  promise5.then(result => console.log(result))
+  promise5.then(result => console.log(result));
+
+
+//    ------------------------------------------CLeanup:finally--------------------------------------
+
+// just liek there is a finally clause in  a regular try{...}  catch{...} there is finally in promises
+
+//  The call .finally(f) is similar to .then(f,f) in the sense that f runs always, when the promise is settled be it resolve or reject.
+
+//  The idea of finally is to set up a handler to performing cleanup/ finalizing after the previous operations are complete
+
+// ex: stopping loading indicators, closing no loner needed connections, etc.
+
+//  Think of it a a party finsher. No matter was a party good or bad. how many frineds were in it. we still need or at least should do a cleanup after it.
+
+
+// There are important differences:
+
+// A finally handler has no arguments. In finally we don’t know whether the promise is successful or not. That’s all right, as our task is usually to perform “general” finalizing procedures.
+
+// Please take a look at the example above: as you can see, the finally handler has no arguments, and the promise outcome is handled by the next handler.
+
+// A finally handler “passes through” the result or error to the next suitable handler.
+
+// For instance, here the result is passed through finally to then:
+
+new Promise((resolve, reject) => {
+  setTimeout(() => resolve("value"), 2000);
+})
+  .finally(() => console.log("Promise ready")) // triggers first
+  .then(result => console.log(result)); 
+
+
+
+//   As you can see, the value returned by the first promise is passed through finally to the next then.
+
+// That’s very convenient, because finally is not meant to process a promise result. As said, it’s a place to do generic cleanup, no matter what the outcome was.
+
+// And here’s an example of an error, for us to see how it’s passed through finally to catch:
+
+
+new Promise((resolve, reject) => {
+    throw new Error("error");
+  })
+    .finally(() => console.log("Promise is  ready")) // triggers first
+    .catch(err => console.log(err));  // <-- .catch shows the error
+  
+//     To summarize:
+
+// A finally handler doesn’t get the outcome of the previous handler (it has no arguments). This outcome is passed through instead, to the next suitable handler.
+// If a finally handler returns something, it’s ignored.
+// When finally throws an error, then the execution goes to the nearest error handle
+
+
+
+//  1 Promises
+
+/* i )  Promises allow us to do things in the natural order. first , we run loadscript(script) and .then we write what to do with the reuslt
+
+
+  ii)  we can call .then on a Promise as many times as we want. Each time, we are adding a new fan, a new subscribing function to the subscription list. more about this in the nest*/
+
+
+
+//   2. callbacks
+
+/*We must have a callback function at our disposal when calling loadscript(scirpt, callback) . In other words, we must know what to do with the result before loadscript is called
+
+There can be only one callback.*/
